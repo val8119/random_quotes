1ceff6f92c9112f463c9b7b34fd6a0d1c08f9315
val8119 <mikk.vahaste@gmail.com>
2020-06-22 15:20:52 +0300

    Cleaned up the code, added favicon

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,11 +1,9 @@
 window.onload = (event) => {
     getQuote()
-    console.log("loaded");
 };
 
 document.getElementById("generate-quote").addEventListener("click", function () {
     getQuote()
-    console.log("click");
 });
 
 function getRandomInt(max) {
@@ -21,13 +19,8 @@ function getQuote() {
 
     request.onload = function () {
         var data = JSON.parse(this.response);
-        console.log(data)
         document.getElementById("quote-body").innerHTML = '"' + data[quoteIndex].text + '"';
         document.getElementById("quote-author").innerHTML = data[quoteIndex].author;
-    }
-
-    if (document.getElementById("quote-author").innerHTML = "") {
-        document.getElementById("quote-author").innerHTML = "Unknown"
     }
 
     request.send();
